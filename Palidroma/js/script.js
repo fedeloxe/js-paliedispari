51ea9f0c212d6c95bc98287279cea2237a17ee02
fedeloxe <loxeee@gmail.com>
2022-12-12 19:02:03 +0100

    bonus form  added

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -1,18 +1,45 @@
-let parola = prompt ('inserisci la parola da controllare ')
+// let parola = prompt ('inserisci la parola da controllare ')
 
 
-function invertiParola(parl){
-    let parolaInversa = parl.split('').reverse('').join('');  
-    return parolaInversa;
-}
+// function invertiParola(parl){
+//     let parolaInversa = parl.split('').reverse('').join('');  
+//     return parolaInversa;
+// }
 
-let parolaInvertita = invertiParola(parola);
+// let parolaInvertita = invertiParola(parola);
 
 
-if(parola == parolaInvertita){
-    console.log('la parola è palindroma');
-} 
+// if(parola == parolaInvertita){
+//     console.log('la parola è palindroma');
+// } 
 
-else {
-    console.log('la parola non è palindroma');
-}
+// else {
+//     console.log('la parola non è palindroma');
+// }
+
+
+//-------------------------------bonus------------------------
+let bottone = document.getElementById('controlla');
+bottone.addEventListener('click', function(){
+
+    let parola = document.getElementById('parola').value 
+
+
+    function invertiParola(parl){
+        let parolaInversa = parl.split('').reverse('').join('');  
+        return parolaInversa;
+    }
+
+    let parolaInvertita = invertiParola(parola);
+
+
+    if(parola == parolaInvertita){
+        document.getElementById('risultato').innerHTML = "la parola è palindroma"
+    } 
+
+    else {
+        document.getElementById('risultato').innerHTML = "la parola non è palindroma"
+    }
+
+})
+
